0fb4291a942e8ca5f4cfcfc546229a715f5d0d22
angelrdelacruz08 <angelrdelacruz.08@gmai.com>
2023-01-28 14:37:32 -0500

    2 commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions:{
+      input: {
+        Index: resolve(__dirname, 'index.html'),
+        pages: resolve(__dirname, 'pages/index.html'),
+      },
+    },
+
+  },
   plugins: [react()],
 })
