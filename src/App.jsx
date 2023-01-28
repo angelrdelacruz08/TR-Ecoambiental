6acacacd7a892d5dedff68f6072f331fc6b8623d
angelrdelacruz08 <angelrdelacruz.08@gmai.com>
2023-01-28 14:59:44 -0500

    5 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import {Route, Routes} from 'react-router-dom'
-import Index from './pages';
+import Home from './pages/Home';
 import Create from './pages/Create';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -14,7 +14,7 @@ function App() {
       <div>
       <Header />
       <Routes>
-        <Route path='/' element={<Index/>} />
+        <Route path='/' element={<Home/>} />
         <Route path='/Create' element={<Create/>} />
       </Routes>
       <Footer />
