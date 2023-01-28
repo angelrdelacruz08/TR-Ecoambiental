dd1db7cd7fe977ddbd900a5e52beb874ef9e8022
angelrdelacruz08 <angelrdelacruz.08@gmai.com>
2023-01-28 15:36:42 -0500

    6 commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ function App() {
       <div>
       <Header />
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/Create' element={<Create/>} />
+        <Route path='/' exact element={<Home/>} />
+        <Route path='/Create' exact element={<Create/>} />
       </Routes>
       <Footer />
       </div>
